69d263b819a123919b5f368f494edfe2639d43fa
Max Knutsson <max.knutsson+demo@dbtlabs.com>
2024-09-26 06:21:13 +0000

    demo and training questions

--- models/staging/iot/stg_iot__iot_sensor_data.sql
+++ models/staging/iot/stg_iot__iot_sensor_data.sql
@@ -9,7 +9,7 @@
 with source as (
     select * from {{ source('iot', 'iot_sensor_data') }}
     {% if is_incremental() %}
-    where unique_id not in (select unique_id from {{ this }}) 
+        where unique_id not in (select unique_id from {{ this }}) 
     {% endif %}
 ),
 
@@ -18,9 +18,7 @@ renamed as (
     select
         *,
         CURRENT_TIMESTAMP() updated_at,
-         '{{ invocation_id }}' as batch_id,
-         1 as newCol1,
-         CURRENT_TIMESTAMP() as newCol3
+         '{{ invocation_id }}' as batch_id
     from source
 )
 
