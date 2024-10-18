f9af7326dc7a7172e74f91b71d0fafd0d2107271
Max Knutsson <max.knutsson+demo@dbtlabs.com>
2024-10-18 08:34:23 +0000

    docs v2

--- models/staging/stg_orders.sql
+++ models/staging/stg_orders.sql
@@ -9,13 +9,21 @@ source as (
 renamed as (
 
     select
+        ----------  ids
         id as order_id,
         store_id as location_id,
         customer as customer_id,
 
-        {{convert_cents_to_euro('subtotal')}} as subtotal_cents,
-        {{convert_cents_to_euro('tax_paid')}} as tax_paid_cents,
-        {{convert_cents_to_euro('order_total')}}  as order_total_cents
+        ---------- numerics
+        subtotal as subtotal_cents,
+        tax_paid as tax_paid_cents,
+        order_total as order_total_cents,
+        {{ cents_to_dollars('subtotal') }} as subtotal,
+        {{ cents_to_dollars('tax_paid') }} as tax_paid,
+        {{ cents_to_dollars('order_total') }} as order_total,
+
+        ---------- timestamps
+        {{ dbt.date_trunc('day','ordered_at') }} as ordered_at
 
     from source
 
